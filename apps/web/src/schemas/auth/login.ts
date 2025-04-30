cda4314b4dda78ad763c314b6874b4cd48bd101f
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-04-30 15:19:44 +0200

    feat(tests): enhance LoginForm tests and add jest setup for testing-library

--- apps/web/src/schemas/auth/login.ts
+++ apps/web/src/schemas/auth/login.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email({ message: "Please enter a valid email address" }),
-  password: z.string().nonempty({ message: "Please enter your password" }),
+  email: z
+    .string()
+    .nonempty({ message: "Email is required" })
+    .email({ message: "Please enter a valid email address" }),
+  password: z.string().nonempty({ message: "Password is required" }),
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
