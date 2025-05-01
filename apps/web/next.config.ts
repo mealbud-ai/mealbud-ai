30c88bfd5548cc2f1cdb1cff0fd19d58d685743d
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-05-01 12:54:06 +0200

    fix(config): add output settings in next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from 'next';
+import path from 'node:path';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@repo/ui"],
+  transpilePackages: ['@repo/ui'],
+  output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, '..', '..'),
 };
 
 export default nextConfig;
