d7447f4df346ebdceb39b2ee66f6fe6c8b080e36
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-05-02 12:37:43 +0200

    feat(user): add user controller and service tests

--- apps/api/src/user/user.service.spec.ts
+++ apps/api/src/user/user.service.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '@repo/db/entities/user';
+import { NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const mockUserRepository = {
+    findOne: jest.fn(),
+  };
+
+  const mockUser: User = {
+    id: 'test-id',
+    email: 'test@example.com',
+    name: 'Test User',
+    avatar_url: null,
+    email_verified: false,
+    created_at: new Date(),
+    accounts: [],
+    meals: [],
+    goals: [],
+    aiRequests: [],
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    service = module.get<UserService>(UserService);
+
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOneById', () => {
+    it('should return a user if user is found', async () => {
+      mockUserRepository.findOne.mockResolvedValueOnce(mockUser);
+
+      const result = await service.findOneById('test-id');
+
+      expect(result).toEqual(mockUser);
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 'test-id' },
+      });
+    });
+
+    it('should throw NotFoundException if user is not found', async () => {
+      mockUserRepository.findOne.mockResolvedValueOnce(null);
+
+      await expect(service.findOneById('non-existent-id')).rejects.toThrow(
+        new NotFoundException('User with ID non-existent-id not found'),
+      );
+      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 'non-existent-id' },
+      });
+    });
   });
 });
