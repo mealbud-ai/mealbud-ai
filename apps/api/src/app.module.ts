4502396df6676e1333ee82b80cbf54b6e050dc4e
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-05-02 12:26:14 +0200

    feat(user): implement user module with controller and service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,44 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { Account } from '@repo/db/entities/account';
+import { User } from '@repo/db/entities/user';
+import { Meal } from '@repo/db/entities/meal';
+import { EmailVerificationToken } from '@repo/db/entities/email-verification-token';
+import { Goal } from '@repo/db/entities/goal';
+import { AIRequest } from '@repo/db/entities/ai-request';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['./.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.NEST_DATABASE_HOST,
+        port: parseInt(process.env.NEST_DATABASE_PORT, 10),
+        username: process.env.NEST_DATABASE_USER,
+        password: process.env.NEST_DATABASE_PASS,
+        database: process.env.NEST_DATABASE_NAME,
+        entities: [
+          User,
+          Meal,
+          Account,
+          EmailVerificationToken,
+          Goal,
+          AIRequest,
+        ],
+        synchronize: process.env.NODE_ENV !== 'production',
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
