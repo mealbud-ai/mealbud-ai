b5499ebe152948199e5f458039b3d4d4b13a09ef
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-05-09 18:56:07 +0200

    test: update snapshot for homepage and mock lucide-react icons

--- apps/web/src/app/__tests__/page.test.tsx
+++ apps/web/src/app/__tests__/page.test.tsx
@@ -2,17 +2,34 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Page from '../page';
 
+jest.mock('lucide-react', () => ({
+  __esModule: true,
+  ArrowUpRightIcon: () => <svg data-testid="arrow-up-right-icon" />,
+  PlayIcon: () => <svg data-testid="play-icon" />,
+  default: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-cookie"
+    >
+      <path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"></path>
+      <path d="M8.5 8.5v.01"></path>
+      <path d="M16 15.5v.01"></path>
+      <path d="M12 12v.01"></path>
+      <path d="M11 17v.01"></path>
+      <path d="M7 14v.01"></path>
+    </svg>
+  ),
+}));
+
 describe('Page', () => {
-  it('renders button text', () => {
-    render(<Page />);
-
-    const button = screen.getByRole('button', {
-      name: /Start Building Now/i,
-    });
-
-    expect(button).toBeInTheDocument();
-  });
-
   it('renders homepage unchanged', () => {
     const { container } = render(<Page />);
 
